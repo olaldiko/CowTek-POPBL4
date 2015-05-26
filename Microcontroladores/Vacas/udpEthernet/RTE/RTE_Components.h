5c21096a7c6592e4cf4758e38fc8ff5f52db8f60
urrutiaitor <aitorurrutiazubikarai@gmail.com>
2015-05-26 19:08:05 +0200

    Version funcional del servidor
    Se ha finalizado la primera version del servidor que funciona.
    Se han cambiado los puertos y también se han quitado las ventanas
    emergentes por avisos por consola.

---
+++ Microcontroladores/Vacas/udpEthernet/RTE/RTE_Components.h
@@ -0,0 +1,38 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'udpEthernet' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+#define RTE_CMSIS_RTOS                  /* CMSIS-RTOS */
+        #define RTE_CMSIS_RTOS_RTX              /* CMSIS-RTOS Keil RTX */
+#define RTE_DEVICE_FRAMEWORK_CLASSIC
+#define RTE_DEVICE_HAL_COMMON
+#define RTE_DEVICE_HAL_CORTEX
+#define RTE_DEVICE_HAL_DMA
+#define RTE_DEVICE_HAL_ETH
+#define RTE_DEVICE_HAL_GPIO
+#define RTE_DEVICE_HAL_PWR
+#define RTE_DEVICE_HAL_RCC
+#define RTE_DEVICE_HAL_SPI
+#define RTE_DEVICE_HAL_USART
+#define RTE_DEVICE_STARTUP_STM32F4XX    /* Device Startup for STM32F4 */
+#define RTE_Drivers_ETH_KSZ8851SNL      /* Driver ETH KSZ8851SNL */
+#define RTE_Drivers_ETH_MAC0            /* Driver ETH_MAC0 */
+#define RTE_Drivers_PHY_DP83848C        /* Driver PHY DP83848C */
+#define RTE_Drivers_PHY_LAN8720         /* Driver PHY LAN8720 */
+#define RTE_Drivers_PHY_ST802RT1        /* Driver PHY ST802RT1 */
+#define RTE_Drivers_SPI1                /* Driver SPI1 */
+        #define RTE_Drivers_SPI2                /* Driver SPI2 */
+        #define RTE_Drivers_SPI3                /* Driver SPI3 */
+        #define RTE_Drivers_SPI4                /* Driver SPI4 */
+        #define RTE_Drivers_SPI5                /* Driver SPI5 */
+        #define RTE_Drivers_SPI6                /* Driver SPI6 */
+
+#endif /* RTE_COMPONENTS_H */
